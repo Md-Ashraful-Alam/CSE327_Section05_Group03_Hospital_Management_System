8a2177a346ccb5ad7c78e8a5e3302db6526f14c6
Md. Ashraful Alam <86886737+Md-Ashraful-Alam@users.noreply.github.com>
2021-12-31 00:35:56 +0600

    Delete Hospital_Management_System directory

--- Hospital_Management_System/Story Table With Doccumentation/source/models.py
+++
@@ -1,9 +0,0 @@
-
-from django.db import models
-from djnago.contrib.auth.models import U 
-
-
-class Story(models.Model):
-    patient_id = models.CharField(max_length=10)
-    story_date = models.DateField()
-    comment = models.CharField(max_length=200)
