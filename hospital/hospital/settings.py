ace09271790be855ddba99a4f801dc07fbe8ed9f
Md. Ashraful Alam <86886737+Md-Ashraful-Alam@users.noreply.github.com>
2022-01-02 00:24:48 +0600

    Update settings.py

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello.apps.HelloConfig',
+    'hello1.apps.HelloConfig',
    
    
 ]
